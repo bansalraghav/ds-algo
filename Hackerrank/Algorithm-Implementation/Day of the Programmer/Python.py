69fc46ce89758b987993e34a88d256bda558b596
Raghav Bansal <virgoraghavbansal@gmail.com>
2018-10-22 05:50:03 +0530

    Day of the programmer added

---
+++ Hackerrank/Algorithm-Implementation/Day of the Programmer/Python.py
@@ -0,0 +1,14 @@
+def dayOfProgrammer(year):
+    if(year == 1918):
+        return "26.09.1918"
+    
+    if(year>=1700 and year<=1917):
+        if(year%4 == 0):
+            return "12.09." + str(year)
+        else:
+            return "13.09."+ str(year)
+    elif(year >1918 and year<=2700):
+        if(year%400 == 0 or (year%4 == 0 and year%100 != 0)):
+            return "12.09." + str(year)
+        else:
+            return "13.09." + str(year)
